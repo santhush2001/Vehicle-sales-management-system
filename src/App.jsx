1a3e9f665c7b6583886a46228d9e0066d5ee993a
santhush2001 <sanjayakody67@gmail.com>
2024-12-21 11:41:55 +0530

    singin
    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-// Component import
+// Component imports
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
@@ -12,10 +13,11 @@ import AppStoreBanner from "./components/AppStoreBanner/AppStoreBanner";
 import Contact from "./components/Contact/Contact";
 import Testimonial from "./components/Testimonial/Testimonial";
 import Footer from "./components/Footer/Footer";
-
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
 
 const App = () => {
-  // dark mode start
+  // Dark mode setup
   const [theme, setTheme] = useState(
     localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
   );
@@ -30,9 +32,9 @@ const App = () => {
       localStorage.setItem("theme", "light");
     }
   }, [theme]);
-  // dark mode end
 
-  React.useEffect(() => {
+  // Initialize AOS for animations
+  useEffect(() => {
     AOS.init({
       offset: 100,
       duration: 800,
@@ -41,30 +43,33 @@ const App = () => {
     });
     AOS.refresh();
   }, []);
+
   return (
-    <div className="bg-white dark:bg-black dark:text-white text-black overflow-x-hidden">
-      <Navbar theme={theme} setTheme={setTheme} />
-      <Hero theme={theme} />
-      <About />
-      <Services />
-      <CarList />
-      <Testimonial />
-      <AppStoreBanner />
-      <Contact />
-      <Footer />
-    </div>
+    <Router>
+      <div className="bg-white dark:bg-black dark:text-white text-black overflow-x-hidden">
+        <Navbar theme={theme} setTheme={setTheme} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero theme={theme} />
+                <About />
+                <Services />
+                <CarList />
+                <Testimonial />
+                <AppStoreBanner />
+                <Contact />
+                <Footer />
+              </>
+            }
+          />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </Router>
   );
-
-
-function App() {
-  return (
-    <div>
-      <CarGallery />
-    </div>
-  );
-}
-
-
 };
 
 export default App;
