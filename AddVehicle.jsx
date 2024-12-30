ecf083fce35a61ff91b378b8a8e939c13a1e65cf
DinithaNipunaka <165243226+DinithaNipunaka@users.noreply.github.com>
2024-12-30 11:10:32 +0530

    Add files via upload

--- AddVehicle.jsx
+++ AddVehicle.jsx
@@ -329,7 +329,7 @@ const AddVehicle = () => {
           </div>
           <button
             type="submit"
-            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-400"
+            className={`mt-4 w-full py-3 text-xl font-semibold rounded-full ${theme === "dark" ? "bg-blue-600 text-white hover:bg-blue-500" : "bg-blue-400 text-black hover:bg-blue-300"}`}
           >
             Add Vehicle
           </button>
